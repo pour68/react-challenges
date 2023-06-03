52907d84e4ead8e7aa8dd349d8adc59bbeda737e
pour68 <pour68@outlook.com>
2023-06-03 12:38:06 +0430

    update cart with context/reducer

--- 12-cart-with-context-and-reducer/src/main.jsx
+++ 12-cart-with-context-and-reducer/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CartProvider } from './contexts/CartContext.jsx'
+import { CartProvider } from './contexts/cartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
